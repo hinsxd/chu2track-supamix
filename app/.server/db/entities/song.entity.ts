dad7d45ed967ff868eb3cfcaf7f86d55a9b1db8f
Alvin Li <hins.xd@hotmail.com>
2024-11-11 18:30:46 +0800

    test

--- app/.server/db/entities/song.entity.ts
+++ app/.server/db/entities/song.entity.ts
@@ -2,7 +2,7 @@ import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
 
 @Entity()
 export class Song {
-  @PrimaryKey()
+  @PrimaryKey({ type: "string" })
   songId!: string;
 
   @Property()
